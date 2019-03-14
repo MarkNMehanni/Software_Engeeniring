a7fbfd199d085fd035ba0dee19d7de133035cb83
Abdelsalaam10 <mohamedabdelsalam30398@gmail.com>
2019-03-14 22:02:54 +0200

    7aget sheriffff

---
+++ Software/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$userAnswer = $_POST['name']; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eavtest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT optionsID, optionsName, optionsHTML
+FROM options
+WHERE optionsID IN (SELECT oID FROM formoptions WHERE fid = '".$userAnswer."')";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br>  ". $row["optionsName"]. " " . $row["optionsHTML"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
+
+</body>
+</html>
