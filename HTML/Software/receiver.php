67e8023f003c78504b3170d0f23628ca8f6d5b20
CrafticalDecoy <CrafticalDecoy@users.noreply.github.com>
2019-03-16 15:06:13 +0200

    nvm

--- HTML/Software/receiver.php
+++ HTML/Software/receiver.php
@@ -49,7 +49,7 @@ class receiver
         $con=new mysqli("localhost","root","","software_engeeniring");
 
         $sql="INSERT INTO `receiver` (`ID`, `Name`, `Email`, `Mobile`, `Telephone`, `Specialty`, `Address`, `Place`, `Category`, `WorkingHours`, `WorkingDays`, `Payment`)
-         VALUES ('$id', '$email', '$mobile', '$telephone', '$specialty', '$address', '$place', '$category', '$workinghours', '$workingdays', '$payment')";
+         VALUES ('$object->id', '$object->email', '$object->mobile', '$object->telephone', '$object->specialty', '$object->address', '$object->place', '$object->category', '$object->workinghours', '$object->workingdays', '$object->payment')";
             
         mysqli_query($con,$sql);
         
